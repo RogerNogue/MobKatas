6000a68d5043e633559dc75bf9116d35303905e0
rogernogue <rogernogueb@gmail.com>
2025-11-16 10:12:29 +0100

    docs: pasted requirements

--- FirstKata/FirstKata.Tests/UnitTest1.cs
+++ FirstKata/FirstKata.Tests/UnitTest1.cs
@@ -1,5 +1,12 @@
 ﻿namespace FirstKata.Tests;
 
+/*
+Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+Any live cell with two or three live neighbours lives on to the next generation.
+Any live cell with more than three live neighbours dies, as if by overcrowding.
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+ */
+
 public class UnitTest1
 {
     [Fact]
