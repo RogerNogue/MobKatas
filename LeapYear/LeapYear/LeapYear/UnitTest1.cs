cd8967fa121ec6057e1bab28d3f3d49e52618858
adrianriverof <adrianriverof@gmail.com>
2025-11-29 10:51:18 +0100

    xwjmwxmj

--- LeapYear/LeapYear/LeapYear/UnitTest1.cs
+++ LeapYear/LeapYear/LeapYear/UnitTest1.cs
@@ -1,5 +1,22 @@
 namespace LeapYear;
 
+
+/*
+       A year is not a leap year if not divisible by 4
+       A year is a leap year if divisible by 4
+       A year is a leap year if divisible by 400
+       A year is not a leap year if divisible by 100 but not by 400
+   Examples:
+       1997 is not a leap year (not divisible by 4)
+       1996 is a leap year (divisible by 4)
+       1600 is a leap year (divisible by 400)
+       1800 is not a leap year (divisible by 4, divisible by 100, NOT divisible by 400)
+ * 
+ */
+
+
+
+
 public class Tests
 {
     [SetUp]
@@ -8,8 +25,21 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void year_not_leap_if_not_divisible_by_4()
     {
-        Assert.Pass();
+        Year sut = new Year(3);
+        
+        
+        
+        Assert.That();
+    }
+    
+}
+
+public class Year
+{
+    public Year(int year)
+    {
+        throw new NotImplementedException();
     }
 }
