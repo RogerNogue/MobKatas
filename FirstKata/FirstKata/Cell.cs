b55ab37507afc52522aa9eb32159380751eaf55f
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2025-12-06 19:57:03 +0100

    dfsafa

--- FirstKata/FirstKata/Cell.cs
+++ FirstKata/FirstKata/Cell.cs
@@ -12,16 +12,15 @@ public readonly struct Cell {
 	public List<Cell> Neighbors()
 	{
 		var cells = new List<Cell>();
-		if (this.Equals(Cell.Origin()))
-		{
-			cells.Add(new Cell(0, 1));
-			cells.Add(new Cell(1, 0));
-			cells.Add(new Cell(1, 1));
-		}
-		else {
-			NewMethod(cells);
-			cells = cells.Where(cell => cell.x >= 0 && cell.y >= 0).ToList();
-		}
+		cells.Add(new Cell(x - 1, y - 1));
+		cells.Add(new Cell(x - 1, y));
+		cells.Add(new Cell(x - 1, y + 1));
+		cells.Add(new Cell(x, y - 1));
+		cells.Add(new Cell(x, y + 1));
+		cells.Add(new Cell(x + 1, y - 1));
+		cells.Add(new Cell(x + 1, y));
+		cells.Add(new Cell(x + 1, y + 1));
+		cells = cells.Where(cell => cell.x >= 0 && cell.y >= 0).ToList();
 		return cells;
 	}
 
