4563e4ec651b60af50e434db6c43c3d5e2db14ea
RogerNogue <rogernogueb@gmail.com>
2025-12-13 19:42:38 +0100

    asghbdfui

--- Potter/Potter/Potter/PotterBook.cs
+++ Potter/Potter/Potter/PotterBook.cs
@@ -5,4 +5,7 @@ public struct PotterBook(string name)
     private string _name = name;
 
     public static implicit operator PotterBook(string name) => new PotterBook(name);
+    
+    public static bool operator ==(PotterBook left, PotterBook right) => left._name == right._name;
+    public static bool operator !=(PotterBook left, PotterBook right) => left._name != right._name;
 }
