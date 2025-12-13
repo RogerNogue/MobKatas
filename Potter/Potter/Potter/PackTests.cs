d6ef88e1c371b058af95e3525192e60ff264e125
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-13 20:34:25 +0100

    aosduihf

---
+++ Potter/Potter/Potter/PackTests.cs
@@ -0,0 +1,91 @@
+﻿namespace Potter;
+
+public class PackTests {
+	[Test]
+	public void BookIsPacked()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("first book");
+		Assert.That(pack, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void TwoBooksInSamePack()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("first book", "fifth book");
+		Assert.That(pack, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void TwoBooksInDifferentPacks()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("first book", "first book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void METHOD()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("second book", "second book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void ThreeBooksInThreePacks()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("second book", "second book", "second book");
+		Assert.That(pack, Is.EqualTo(3));
+	}
+
+	[Test]
+	public void ThreeBooksInTwoPacks()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("second book", "second book", "first book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void TwoPacksOfTwoBooks()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("second book", "first book", "second book", "first book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void ThreeBooksSamePack()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("first book", "second book", "third book");
+		Assert.That(pack, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void cccccccccc()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks("fifth book", "first book", "second book", "first book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+
+	[Test]
+	public void cccccccccch()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks( "first book", "second book", "fourth book", "third book");
+		Assert.That(pack, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void TestForThree()
+	{
+		var sut = new ShoppingCart();
+		int pack = sut.GetAmountOfPacks( "first book", "third book", "third book");
+		Assert.That(pack, Is.EqualTo(2));
+	}
+}
