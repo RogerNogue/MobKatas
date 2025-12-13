a1f2e04260905cad309774999afdf394d0c0d042
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-13 18:21:14 +0100

    oisuadhf

--- Potter/Potter/Potter/UnitTest1.cs
+++ Potter/Potter/Potter/UnitTest1.cs
@@ -1,12 +1,17 @@
 namespace Potter;
 
+// Copia: 8€
+//
+// 2 -> 5%
+// 3 -> 10%
+// 4 -> 20%
+// 5 -> 25%
+//
+// Tienen que ser distintos
+// Los descuentos aplican por sets
+
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
