b16cf5b61261508e0f396c0882ea8c80aaab4c2c
RogerNogue <rogernogueb@gmail.com>
2025-12-13 19:12:11 +0100

    gfyhwebru

--- Potter/Potter/Potter/ShoppingCart.cs
+++ Potter/Potter/Potter/ShoppingCart.cs
@@ -3,10 +3,24 @@ namespace Potter;
 public class ShoppingCart
 {
     private PotterBook[] _books = [];
+    private List<PotterPack> packs = [new PotterPack()];
 
     public void Add(params PotterBook[] books)
     {
         _books = books;
+        foreach (var book in books)
+        {
+            foreach (var pack in packs)
+            {
+                if (pack.Contains(book))
+                {
+                    continue;
+                }
+                pack.Add(book);
+                break;
+            }
+            
+        }
     }
 
     public float Price()
@@ -29,5 +43,18 @@ public class ShoppingCart
         }
 
         return 8 * _books.Length;
+    }
+}
+
+internal class PotterPack
+{
+    public bool Contains(PotterBook book)
+    {
+        return false;
+    }
+
+    public void Add(PotterBook book)
+    {
+        
     }
 }
