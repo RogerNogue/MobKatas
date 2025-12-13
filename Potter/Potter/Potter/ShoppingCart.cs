c7a49253d54f29002bec1b3b053ab02d2d7dd583
adrianriverof <adrianriverof@gmail.com>
2025-12-13 18:57:14 +0100

    add braces {}

--- Potter/Potter/Potter/ShoppingCart.cs
+++ Potter/Potter/Potter/ShoppingCart.cs
@@ -12,9 +12,15 @@ public class ShoppingCart
     public float Price()
     {
         if (_books.Contains("third book"))
+        {
             return 21.6f;
+        }
+
         if (_books.Contains("second book"))
+        {
             return 15.2f;
+        }
+
         return 8 * _books.Length;
     }
 }
