c11ea43c78a200d4f57a2dd83107bef674e1d312
Gerard Gascón <ggasconmoline@gmail.com>
2026-01-18 11:34:35 +0100

    poaisdfhj

--- Potter/Potter/Potter/Pack.cs
+++ Potter/Potter/Potter/Pack.cs
@@ -5,8 +5,10 @@ namespace Potter;
 public class Pack : IEnumerable<PotterBook> {
     public List<PotterBook> Books = [];
 
-    public void Add(PotterBook firstBook) {
-        Books.Add(firstBook);
+    public void Add(PotterBook book) {
+        if (Books.Contains(book))
+            throw new InvalidOperationException("Book already exists");
+        Books.Add(book);
     }
 
     public IEnumerator<PotterBook> GetEnumerator() {
