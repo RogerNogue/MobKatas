498363dcc52982011e32c1295f09237b397d9c3d
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-02-07 11:59:16 +0100

    test: parameterize test

--- Carpeta/Katas/Katas/UnitTest1.cs
+++ Carpeta/Katas/Katas/UnitTest1.cs
@@ -27,62 +27,22 @@ Number	Numeral
 
 public class Tests
 {
-    [Test]
-    public void Convert1ShouldReturnI()
-    {
-        Assert.That(RomanMatematician.Convert(1), Is.EqualTo(I));
-    }
+    static List<(int arabic, RomanNumber expectedConversion)> testCases = new() {
+        (1, I),
+        (2, I.Concat(I)),
+        (3, I.Concat(I).Concat(I)),
+        (4, I.Concat(V)),
+        (5, V),
+        (6, V.Concat(I)),
+        (7, V.Concat(I).Concat(I)),
+        (8, V.Concat(I).Concat(I).Concat(I)),
+        (9, I.Concat(X)),
+        (10, X)
+    };
 
-    [Test]
-    public void Convert2ShouldReturnII()
-    {
-        Assert.That(RomanMatematician.Convert(2), Is.EqualTo(I.Concat(I)));
-    }
-
-    [Test]
-    public void Convert3ShouldReturnIII() 
-    {
-        Assert.That(RomanMatematician.Convert(3), Is.EqualTo(I.Concat(I).Concat(I)));    
-    }
-
-    [Test]
-    public void Convert4ShouldReturnIV() {
-        Assert.That(RomanMatematician.Convert(4), Is.EqualTo(I.Concat(V)));
-    }
-
-    [Test]
-    public void Convert5ShouldReturnV() {
-        Assert.That(RomanMatematician.Convert(5), Is.EqualTo(V));
-    }
-
-    [Test]
-    public void Convert6ShouldReturnVI()
-    {
-        Assert.That(RomanMatematician.Convert(6), Is.EqualTo(V.Concat(I)));
-    }
-
-    [Test]
-    public void Convert7ShouldReturnVII()
-    {
-        Assert.That(RomanMatematician.Convert(7), Is.EqualTo(V.Concat(I).Concat(I)));
-    }
-
-    [Test]
-    public void Convert8ShouldReturnVIII()
-    {
-        Assert.That(RomanMatematician.Convert(8), Is.EqualTo(V.Concat(I).Concat(I).Concat(I)));
-    }
-
-    [Test]
-    public void Convert9ShouldReturnIX()
-    {
-        Assert.That(RomanMatematician.Convert(9), Is.EqualTo(I.Concat(X)));
-    }
-
-    [Test]
-    public void Convert10ShouldReturnX()
-    {
-        Assert.That(RomanMatematician.Convert(10), Is.EqualTo(X));
+    [TestCaseSource(nameof(testCases))]
+    public void ConvertArabicToRoman((int arabic, RomanNumber expected) testCase) {
+        Assert.That(RomanMatematician.Convert(testCase.arabic), Is.EqualTo(testCase.expected));
     }
 }
 
