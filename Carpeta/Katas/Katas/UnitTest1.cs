6ada85e88dfb66a4c18dd002c226721c60e9cc02
RogerNogue <rogernogueb@gmail.com>
2026-02-07 10:23:01 +0100

    docs: reference table for decimal-roman conversion

--- Carpeta/Katas/Katas/UnitTest1.cs
+++ Carpeta/Katas/Katas/UnitTest1.cs
@@ -1,12 +1,25 @@
 namespace Katas;
+/*
+ * 
+Number	Numeral
+1	I
+5	V
+10	X
+50	L
+100	C
+500	D
+1000 M
 
+
+Number	Numeral
+1	    I
+2	    II
+3	    III
+4	    IV
+
+ */
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
