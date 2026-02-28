df7086e97a8ff81c3e83f446a8da28f03a58b4cb
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-02-28 18:26:22 +0100

    test: test file with requirements

---
+++ FirstKata/FirstKata.Tests/Stack/StackTests.cs
@@ -0,0 +1,20 @@
+namespace FirstKata.Tests.Stack;
+
+// In computer science, a stack is a famous abstract data type that provides certain operations on a collection of elements.
+// Push - Add an element to the top of the stack    
+// Pop - Remove an element from the top of the stack, returning it
+// Empty check - Check if the stack is empty or not
+// Size - Count of the elements in the stack
+// Peek - Check the top of the stack without popping
+
+// Handle overflows when too many elements are pushed to the stack
+// Handle underflows when too many elements are popped off the stack
+// Handle underflows when there are no elements to peek on the stack
+// Handle attempts to create a stack with an invalid capacity (negative numbers)
+
+public class StackTests {
+    [Fact]
+    public void Adfas() {
+        
+    }
+}
