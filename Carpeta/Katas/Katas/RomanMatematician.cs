d1d1c5074788305f26cba0ce14915853108719cb
RogerNogue <rogernogueb@gmail.com>
2026-03-11 17:36:59 +0100

    refactor: imported static RomanNumber

--- Carpeta/Katas/Katas/RomanMatematician.cs
+++ Carpeta/Katas/Katas/RomanMatematician.cs
@@ -1,19 +1,21 @@
+using static Katas.RomanNumber;
+
 namespace Katas;
 
 public static class RomanMatematician
 {
     static Dictionary<int, RomanNumber> conversions = new Dictionary<int, RomanNumber>()
     {
-        {1, RomanNumber.I},
-        {2, RomanNumber.I.Concat(RomanNumber.I)},
-        {3, RomanNumber.I.Concat(RomanNumber.I).Concat(RomanNumber.I)},
-        {4, RomanNumber.I.Concat(RomanNumber.V)},
-        {5, RomanNumber.V},
-        {6, RomanNumber.V.Concat(RomanNumber.I)},
-        {7, RomanNumber.V.Concat(RomanNumber.I).Concat(RomanNumber.I)},
-        {8, RomanNumber.V.Concat(RomanNumber.I).Concat(RomanNumber.I).Concat(RomanNumber.I)},
-        {9, RomanNumber.I.Concat(RomanNumber.X)},
-        {10, RomanNumber.X}
+        {1, I},
+        {2, I.Concat(I)},
+        {3, I.Concat(I).Concat(I)},
+        {4, I.Concat(V)},
+        {5, V},
+        {6, V.Concat(I)},
+        {7, V.Concat(I).Concat(I)},
+        {8, V.Concat(I).Concat(I).Concat(I)},
+        {9, I.Concat(X)},
+        {10, X}
     };
     
     public static RomanNumber Convert(int arabicNumber)
