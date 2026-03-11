16588c4f384605e066ffd315ed0d9c8786e8dfb9
RogerNogue <rogernogueb@gmail.com>
2026-03-11 18:11:07 +0100

    refactor: deleted unused structure

--- Carpeta/Katas/Katas/ArabicToRoman.cs
+++ Carpeta/Katas/Katas/ArabicToRoman.cs
@@ -4,20 +4,6 @@ namespace Katas;
 
 public static class ArabicToRoman
 {
-    static Dictionary<int, RomanNumber> conversions = new Dictionary<int, RomanNumber>()
-    {
-        {1, I},
-        {2, I.Concat(I)},
-        {3, I.Concat(I).Concat(I)},
-        {4, I.Concat(V)},
-        {5, V},
-        {6, V.Concat(I)},
-        {7, V.Concat(I).Concat(I)},
-        {8, V.Concat(I).Concat(I).Concat(I)},
-        {9, I.Concat(X)},
-        {10, X}
-    };
-    
     static Dictionary<int, RomanNumber> primitives = new Dictionary<int, RomanNumber>()
     {
         {1, I},
