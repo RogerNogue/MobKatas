3288bd218713035a41d2af9854f8093b2a5b21e2
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-03-22 10:21:16 +0100

    docs: rpg requirements

---
+++ Carpeta/Katas/Katas/RPG/RPGTests.cs
@@ -0,0 +1,19 @@
+namespace Katas.RPG;
+
+// - Health, starting at 1000
+//     - Level, starting at 1
+//     - May be Alive or Dead, starting Alive (Alive may be a true/false)
+
+// - Damage is subtracted from Health
+//     - When damage received exceeds current Health, Health becomes 0 and the character dies
+
+// A Character can Heal a Character.
+//     - Dead characters cannot be healed
+//     - Healing cannot raise health above 1000
+
+public class RPGTests {
+    [Test]
+    public void METHOD() {
+        
+    }    
+}
