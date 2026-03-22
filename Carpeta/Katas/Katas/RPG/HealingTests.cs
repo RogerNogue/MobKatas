de137d36bdfb81667706d39b8016947a932cd4a6
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-03-22 11:42:29 +0100

    test: refactor tests to heal itself

--- Carpeta/Katas/Katas/RPG/HealingTests.cs
+++ Carpeta/Katas/Katas/RPG/HealingTests.cs
@@ -8,57 +8,56 @@ public class HealingTests
     [Test]
     public void HealAnotherCharacter() 
     {
-        var sut = Character.Create(damage: healing, healing: healing);
-        var victim = Character.Create();
-        sut.Attack(victim);
+        var attacker = Character.Create(damage: healing);
+        var sut = Character.Create(healing: healing);
+        attacker.Attack(sut);
         
-        sut.Heal(victim);
+        sut.Heal(sut);
         
-        Assert.That(victim.Health, Is.EqualTo(initialHealth));
+        Assert.That(sut.Health, Is.EqualTo(initialHealth));
     }
 
     [Test]
     public void HealthIsCappedAtInitialHealth()
     {
         var sut = Character.Create(healing: healing);
-        var victim = Character.Create();
         
-        sut.Heal(victim);
+        sut.Heal(sut);
         
-        Assert.That(victim.Health, Is.EqualTo(initialHealth));
+        Assert.That(sut.Health, Is.EqualTo(initialHealth));
     }
 
     [Test]
     public void CannotHealDeadCharacter() {
-        var sut = Character.Create(damage: initialHealth);
-        var victim = Character.Create(health: initialHealth);
+        var attacker = Character.Create(damage: initialHealth);
+        var sut = Character.Create(health: initialHealth);
         
-        sut.Attack(victim);
+        attacker.Attack(sut);
         
-        Assert.That(sut.CanHeal(victim), Is.False);
+        Assert.That(sut.CanHeal(sut), Is.False);
     }
 
     [Test]
     public void CanHealDamagedCharacters() {
-        var sut = Character.Create();
-        var victim = Character.Create(health: initialHealth);
+        var attacker = Character.Create();
+        var sut = Character.Create(health: initialHealth);
         
-        sut.Attack(victim);
+        attacker.Attack(sut);
         
-        Assert.That(sut.CanHeal(victim), Is.True);
+        Assert.That(sut.CanHeal(sut), Is.True);
     }
 
     [Test]
     public void AccumulativeHealing()
     {
-        var sut = Character.Create();
-        var victim = Character.Create(health: initialHealth);
-        sut.Attack(victim);
-        sut.Attack(victim);
+        var attacker = Character.Create();
+        var sut = Character.Create(health: initialHealth);
+        attacker.Attack(sut);
+        attacker.Attack(sut);
         
-        sut.Heal(victim);
-        sut.Heal(victim);
+        sut.Heal(sut);
+        sut.Heal(sut);
         
-        Assert.That(victim.Health, Is.EqualTo(initialHealth));
+        Assert.That(sut.Health, Is.EqualTo(initialHealth));
     }
 }
