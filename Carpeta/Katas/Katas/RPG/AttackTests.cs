928249e70c47c92dffa20b700e46d84620d91b73
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-03-22 11:44:05 +0100

    docs: update requirements

--- Carpeta/Katas/Katas/RPG/AttackTests.cs
+++ Carpeta/Katas/Katas/RPG/AttackTests.cs
@@ -1,6 +1,5 @@
 namespace Katas.RPG;
 /*
-    - A Character can only Heal itself.
     - When dealing damage: - If the target is 5 or more Levels above the attacker, Damage is reduced by 50% - If the target is 5 or more Levels below the attacker, Damage is increased by 50%
  */
 public class AttackTests {
