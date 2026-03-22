a2c314177ef44c848cf41789db5ca088963cb1b6
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-03-22 10:50:54 +0100

    refactor: extract class

--- Carpeta/Katas/Katas/RPG/RPGTests.cs
+++ Carpeta/Katas/Katas/RPG/RPGTests.cs
@@ -65,28 +65,3 @@ public class RPGTests {
         Assert.That(victim.IsAlive, Is.True);
     }
 }
-
-public class Character {
-    public bool IsAlive { get; set; } = true;
-    public int Health { get; private set; }
-    readonly int damage;
-
-    Character(int health, int damage) {
-        Health = health;
-        this.damage = damage;
-    }
-
-    public void Attack(Character victim)
-    {
-        victim.ReceiveDamage(damage);
-    }
-
-    void ReceiveDamage(int damage) {
-        Health -= damage;
-        IsAlive = Health > 0;
-    }
-
-    public static Character Create(int health = 1000, int damage = 1) {
-        return new Character(health, damage);
-    }
-}
